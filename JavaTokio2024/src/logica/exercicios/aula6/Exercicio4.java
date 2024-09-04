b0c0793e48a7d3f2be150b233b1da099504dd696
Gabss1910 <gapo1910@gmail.com>
2024-09-04 17:54:34 -0300

    commit

--- JavaTokio2024/src/logica/exercicios/aula6/Exercicio4.java
+++ JavaTokio2024/src/logica/exercicios/aula6/Exercicio4.java
@@ -1,10 +1,27 @@
 package logica.exercicios.aula6;
 
+import java.util.Scanner;
+
 public class Exercicio4 {
 
 	public static void main(String[] args) {
 		
+		
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Digite o primeiro valor (A): ");
+        int A = sc.nextInt();
+
+        System.out.print("Digite o segundo valor (B): ");
+        int B = sc.nextInt();
+
+        sc.close();
+
+        if (A % B == 0 || B % A == 0) {
+            System.out.println("São Múltiplos");
+        } else {
+            System.out.println("Não são Múltiplos");
+
+        }
 	}
-
 }
---
+++ JavaTokio2024/src/logica/exercicios/aula6/Exercicio7.java
@@ -0,0 +1,11 @@
+package logica.exercicios.aula6;
+
+public class Exercicio7 {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
